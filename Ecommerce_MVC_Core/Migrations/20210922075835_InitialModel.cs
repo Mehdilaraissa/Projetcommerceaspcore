6d9c3dd44469bec13b957e4f30f417ffa98f768b
Mehdi <laraissa1995@gmail.com>
2021-09-22 14:24:01 +0100

    first commit

---
+++ Ecommerce_MVC_Core/Migrations/20210922075835_InitialModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ecommerce_MVC_Core.Migrations
+{
+    public partial class InitialModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AddedDate",
+                table: "ProductLikes",
+                nullable: false,
+                defaultValue: new DateTime(2021, 9, 22, 8, 58, 35, 774, DateTimeKind.Local),
+                oldClrType: typeof(DateTime),
+                oldDefaultValue: new DateTime(2021, 3, 27, 12, 4, 17, 693, DateTimeKind.Local));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AddedDate",
+                table: "OrderStatus",
+                nullable: false,
+                defaultValue: new DateTime(2021, 9, 22, 8, 58, 35, 785, DateTimeKind.Local),
+                oldClrType: typeof(DateTime),
+                oldDefaultValue: new DateTime(2021, 3, 27, 12, 4, 17, 710, DateTimeKind.Local));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AddedDate",
+                table: "ProductLikes",
+                nullable: false,
+                defaultValue: new DateTime(2021, 3, 27, 12, 4, 17, 693, DateTimeKind.Local),
+                oldClrType: typeof(DateTime),
+                oldDefaultValue: new DateTime(2021, 9, 22, 8, 58, 35, 774, DateTimeKind.Local));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AddedDate",
+                table: "OrderStatus",
+                nullable: false,
+                defaultValue: new DateTime(2021, 3, 27, 12, 4, 17, 710, DateTimeKind.Local),
+                oldClrType: typeof(DateTime),
+                oldDefaultValue: new DateTime(2021, 9, 22, 8, 58, 35, 785, DateTimeKind.Local));
+        }
+    }
+}
